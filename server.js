53778a97b15e0a32cc65e71c4ac8b7380019d214
krpraveen0 <pkec151015@gmail.com>
2021-11-30 23:21:53 +0530

    first get route added

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 
+
+const mockUser = [
+    {name:'Praveen'},
+    {name:'Raju'},
+    {name:'Munna'}
+]
+
+app.get('/users',function(req,res) {
+    res.json(
+        {
+            success:'true',
+            message: 'successfully fetched all the users!',
+            users: mockUser
+        }
+    )
+})
+
 app.listen(8000,function() {
     console.log("app is running on port 8000")
 })
